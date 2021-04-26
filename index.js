00123705254059d1b509520a276eafcfd4b90444
Raghda  Muayad <therose000777@gmail.com>
2021-04-26 13:08:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+const Cards = ["Lisa","Kaitlin","Jan"];
+function  writeCards(Cards) {
+  for (let i = 0; i < Cards.length; i++) {
+    console.log(`Thank you,${Cards[i]}, for the wonderful surprise gift!`);
+
+  }
+
+
+  return writeCards;
+}
+
+ writeCards(Cards);
+
+countDown (4);
