8f71e0689c2593532d6556cea2fa917c4dd69ee2
Raghda  Muayad <therose000777@gmail.com>
2021-04-26 13:34:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const Cards = ["Lisa","Kaitlin","Jan"];
+function  writeCards(Cards) {
+const  newarray=[];
+  for (let i = 0; i < Cards.length; i++) {
+    newarray.push (`Thank you, ${Cards[i]}, for the wonderful surprise gift!`);
+
+  }
+
+
+  return newarray ;
+}
+
+ writeCards(Cards);
+function  countDown(max) {
+
+  for (let i = max; i >= 0; i--) {
+    console.log(i);
+  }
+}
+countDown (10);
